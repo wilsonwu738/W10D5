dec3ef6985aecde3ff93867bba6b9eb5a82b17e2
Wilson Wu <wuyecheng738@gmail.com>
2023-01-20 15:34:29 -0500

    props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import harvardArt from './data/harvardArt.js'
+import GalleryNavigation from './components/index.js';
+
 function App() {
+  // console.log(harvardArt)
   return (
-    <h1>Hello from App</h1>
+    // <h1>Hello from App</h1>
+    <GalleryNavigation galleries={harvardArt.records}/>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom"
+
+
+function GalleryNavigation(props) {
+  console.log(props)
+  return(
+    <nav>
+      <h1>hi Luis</h1>
+      <NavLink to="/">
+
+      </NavLink>
+    </nav>
+  )
+}
+
+export default GalleryNavigation
