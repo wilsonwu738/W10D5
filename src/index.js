12c30a94fdca59e02c49eb1ab9110856b35b6f49
Wilson Wu <wuyecheng738@gmail.com>
2023-01-20 14:44:42 -0500

    harvard data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
+import { BrowserRouter }from 'react-router-dom'
+
+function Root() {
+  return (
+    // <h1>Hello from Root</h1>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  );
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
-    {/* <h1>Hello from Root</h1> */}
+    <Root/>
   </React.StrictMode>,
   document.getElementById('root')
 );
